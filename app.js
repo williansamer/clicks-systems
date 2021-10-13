4a1d0e95d19e6aaa50b484e7f0edfb23b470366d
williansamer <willian.samer@gmail.com>
2021-10-13 10:39:57 -0300

    criação do MongoDb Atlas e conectando com o Heroku

--- app.js
+++ app.js
@@ -5,10 +5,12 @@ const ejs = require("ejs");
 const mongoose = require("mongoose");
 const routerLink = require("./routes/linkroutes");
 const dotenv = require("dotenv");
+const cors = require("cors");
 
 dotenv.config();
+app.use(cors());
 
-mongoose.connect("mongodb://127.0.0.1:27017/users")
+mongoose.connect(process.env.MONGO_URL)
     .then(()=>{
         app.set("view engine", "ejs");
         app.set("views", path.join(__dirname, "templates"));
@@ -21,3 +23,5 @@ mongoose.connect("mongodb://127.0.0.1:27017/users")
 app.listen(process.env.PORT || 3000, ()=>{
     console.log("Server running..")
 })
+
+//MONGO EM PRODUÇÃO: mongodb+srv://deploy:redirectlinks@cluster0.ggg3m.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
